332c0ecd057dfa1f9a5dcc12cb7e3d001d944880
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-06-21 11:09:30 +0700

    modified:   resources/views/home/index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,45 +1,51 @@
 @extends('layouts.app')
 
-@section('styles')
+@section('content')
+
     <style>
         html,
         body {
             overflow-x: hidden;
+        }
+
+        .btn {
+            white-space: nowrap;
+        }
+
+        .img-fluid,
+        img {
             max-width: 100%;
+            height: auto;
+        }
+
+        .ratio-16x9 {
+            aspect-ratio: 16 / 9;
         }
 
         .object-fit-cover {
             object-fit: cover;
         }
     </style>
-@endsection
 
-@section('content')
     <div style="height: 100vh; background-image: url(https://images.unsplash.com/photo-1619441207978-3d326c46e2c9?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D); background-size: cover; background-position: center;"
         class="position-relative w-100 mb-5">
         <div class="position-absolute text-white d-flex flex-column align-items-center justify-content-center"
             style="top: 0; right: 0; bottom: 0; left: 0; background-color: rgba(0, 0, 0, 0.5);">
             <span>Gallery App</span>
-            <h1 class="mb-4 mt-2 fw-bold text-center">Made with Laravel Framework</h1>
+            <h1 class="mb-4 mt-2 font-weight-bold text-center">Made with Laravel Framework</h1>
             <div class="text-center">
                 @if (auth()->check())
-                    <a href="{{ route('album.create') }}" class="btn px-4 py-2 text-white mx-1"
-                        style="border-radius: 30px; background-color: #4346eb;">
-                        <i class="fa-solid fa-plus"></i> Add Album
-                    </a>
-                    <a href="{{ route('foto.create') }}" class="btn px-4 py-2 text-white mx-1"
-                        style="border-radius: 30px; border: 1px solid #4346eb;">
-                        <i class="fa-solid fa-plus"></i> Add Photo
-                    </a>
+                    <a href="{{ route('album.create') }}" class="btn btn-sm px-3 py-2 text-white mt-3 mt-sm-0 mx-1"
+                        style="border-radius: 30px; background-color: #4346eb;"><i class="fa-solid fa-plus"></i> Add Album</a>
+                    <a href="{{ route('foto.create') }}" class="btn btn-sm px-3 py-2 text-white mt-3 mt-sm-0 mx-1"
+                        style="border-radius: 30px; border:1px solid #4346eb;"><i class="fa-solid fa-plus"></i> Add Photo</a>
                 @else
-                    <a href="{{ route('login') }}" class="btn px-4 py-2 text-white mx-1"
-                        style="border-radius: 30px; background-color: #4346eb;">
-                        <i class="fa-solid fa-right-to-bracket"></i> Login
-                    </a>
-                    <a href="{{ route('register.create') }}" class="btn px-4 py-2 text-white mx-1"
-                        style="border-radius: 30px; border:1px solid #4346eb;">
-                        <i class="fa-solid fa-user-plus"></i> Register
-                    </a>
+                    <a href="{{ route('login') }}" class="btn btn-sm px-3 py-2 text-white mt-3 mt-sm-0 mx-1"
+                        style="border-radius: 30px; background-color: #4346eb;"><i class="fa-solid fa-right-to-bracket"></i>
+                        Login</a>
+                    <a href="{{ route('register.create') }}" class="btn btn-sm px-3 py-2 text-white mt-3 mt-sm-0 mx-1"
+                        style="border-radius: 30px; border:1px solid #4346eb;"><i class="fa-solid fa-user-plus"></i>
+                        Register</a>
                 @endif
             </div>
         </div>
@@ -50,7 +56,9 @@
         <form action="{{ route('home.index') }}#images" method="GET" class="col-lg-6">
             <div class="input-group">
                 <input type="text" name="query" class="form-control" placeholder="Search photos...">
-                <button type="submit" class="btn btn-primary"><i class="fa-solid fa-magnifying-glass"></i></button>
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-primary"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
             </div>
         </form>
     </div>
@@ -65,7 +73,9 @@
                         <option value="{{ $albumName }}">{{ $albumName }}</option>
                     @endforeach
                 </select>
-                <button type="submit" class="btn btn-primary">Search Albums</button>
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-primary">Search Albums</button>
+                </div>
             </div>
         </form>
     </div>
@@ -76,23 +86,21 @@
         </center>
     @endif
 
-    <div class="container col-xxl-8 px-4 py-5">
-        @if ($fotos->count())
-            <div class="row g-4">
+    <div class="container-lg px-3 py-4">
+        @if (count($fotos) > 0)
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                 @foreach ($fotos as $foto)
-                    <div class="col-lg-4">
+                    <div class="col-lg-4 mb-4">
                         <a href="{{ route('home.show', $foto->id) }}" class="text-decoration-none">
                             <div class="border rounded p-3">
                                 <div class="ratio ratio-16x9 mb-3">
-                                    <img src="{{ Storage::url($foto->LokasiFile) }}?quality=20"
-                                        class="w-100 h-100 object-fit-cover rounded" alt="{{ $foto->JudulFoto }}" loading="lazy">
+                                    <img src="{{ Storage::url($foto->LokasiFile) }}?quality=10"
+                                        class="object-fit-cover w-100 h-100 rounded" alt="{{ $foto->JudulFoto }}" loading="lazy">
                                 </div>
-                                <h5 class="mb-2">{{ Str::limit($foto->JudulFoto, 25) }}</h5>
+                                <h3 class="mb-2">{{ Str::limit($foto->JudulFoto, 25) }}</h3>
                                 <p class="mb-2">{{ Str::limit($foto->DeskripsiFoto, 40) }}</p>
                                 <p class="mb-0">Uploaded by: {{ $foto->user->Username }}</p>
-                                <p class="mt-2 text-muted">
-                                    <i class="fa-solid fa-heart"></i> {{ $foto->total_likes }}
-                                    &nbsp;&nbsp;
+                                <p class="mt-2"><i class="fa-solid fa-heart"></i> {{ $foto->total_likes }}&nbsp;&nbsp;&nbsp;
                                     <i class="fa-solid fa-comment"></i> {{ $foto->komentar_count }}
                                 </p>
                             </div>
@@ -101,9 +109,9 @@
                 @endforeach
             </div>
         @else
-            <div class="text-center">
-                <p class="fw-bold">No image was uploaded.</p>
-                <p class="fw-bold">¯\_(ツ)_/¯</p>
+            <div class="col-12 text-center">
+                <p class="font-weight-bold">No image was uploaded.</p>
+                <p class="font-weight-bold">¯\_(ツ)_/¯</p>
             </div>
         @endif
     </div>
