e17729278b1950976f19a916b49ce2b13ae22127
ShirokamiRyzen <fatihfirdaus404@gmail.com>
2025-07-11 18:10:03 +0700

    modified:   resources/views/layouts/footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -46,12 +46,12 @@
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-                    <p>
+                    <!-- <p>
                         <a class="text-reset text-decoration-none" href="https://t.me/ShirokamiRyzen">
                             <i class="fa-brands fa-telegram me-3"></i>
                             Shirokami Ryzen
                         </a>
-                    </p>
+                    </p> -->
                     <p>
                         <a class="text-reset text-decoration-none" href="mailto:fatihfirdaus404@gmail.com">
                             <i class="fa-solid fa-envelope me-3"></i>
